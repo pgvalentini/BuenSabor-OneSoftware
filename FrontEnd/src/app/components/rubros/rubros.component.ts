b11b332411dae294a08d28d504a9f23317dc254e
Pablo Valentini <pablogastonvalentini@gmail.com>
2022-07-28 20:37:02 -0300

    Creación de los rubros

---
+++ FrontEnd/src/app/components/rubros/rubros.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rubros',
+  templateUrl: './rubros.component.html',
+  styles: [
+  ]
+})
+export class RubrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
